a169f8ebe8b6dbfe3e26640ae2cf396b1bc7ee30
nikhilkhanna <nikhilkh@gmail.com>
2014-01-12 22:02:28 -0800

    fuck da polic

--- myfunction.hs
+++ myfunction.hs
@@ -258,3 +258,101 @@ type PhoneBook = [(Name,PhoneNumber)]
 
 inPhoneBook :: Name -> PhoneNumber -> PhoneBook -> Bool
 inPhoneBook name pnumber pbook = (name,pnumber) `elem` pbook
+
+-- THE LOCKER LOOKUP PROGRAM--
+
+data LockerState = Taken | Free deriving (Show, Eq)
+type Code = String
+type Error = String
+type LockerMap = Map.Map Int (LockerState, Code)
+
+lockerLookup :: Int -> LockerMap -> Either Error Code
+lockerLookup lockerNumber map = 
+	case Map.lookup lockerNumber map of
+		Nothing -> Left $ "Locker Number" ++ show lockerNumber ++ "doesn't exist"
+		Just (state, code) -> if state /= Taken
+								then Right code
+								else Left $ "Locker " ++ show lockerNumber ++ "Is already taken"
+
+lockers :: LockerMap  
+lockers = Map.fromList   
+    [(100,(Taken,"124"))  
+    ,(101,(Free,"231"))  
+    ,(103,(Free,"234"))  
+    ]  
+
+-- MAKING MY OWN LIST--
+infixr 5 :-:
+
+data List a = Empty | a :-: (List a) deriving (Show,Read,Eq,Ord)
+
+infixr 5 .++ 
+
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+--MAKING MY OWN BINARY SEARCH TREE-- 
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show,Read,Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+	| x == a = Node x left right
+	| x < a = Node a (treeInsert x left) right
+	| x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool
+treeElem x EmptyTree = False
+treeElem x (Node a left right)
+	| x == a = True
+	| x < a = treeElem x left
+	| x > a = treeElem x right
+
+--INSTANCIATING A TYPECLASS --
+data TrafficLight = Red | Yellow | Green
+
+instance Eq TrafficLight where
+		Red == Red = True
+		Green == Green = True
+		Yellow == Yellow = True
+		_ == _ = False
+
+instance Show TrafficLight where
+	show Red = "Red light"
+	show Yellow = "Yellow light"
+	show Green = "Green light"
+
+--YES NO TYPECLASS -- 
+class YesNo a where
+	yesno :: a -> Bool
+
+
+instance YesNo [a] where
+	yesno [] = False
+	yesno _ = True 
+
+instance YesNo Bool where
+	yesno = id
+
+instance YesNo (Maybe a) where
+	yesno (Just _) = True
+	yesno Nothing = False 
+
+instance YesNo (Tree a) where
+	yesno EmptyTree = False
+	yesno _ = True
+
+instance YesNo TrafficLight where
+	yesno Red = False
+	yesno _ = True 
+
+instance YesNo Int where
+	yesno 0 = False
+	yesno _ = True
+
+yesnoIf:: (YesNo y) => y -> a -> a -> a
+yesnoIf yesnoVal yesResult noResult = if yesno yesnoVal then yesResult else noResult 
